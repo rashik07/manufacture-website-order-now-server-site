793def94757db53b3cbeca1f07f6c9e251b60b05
rashik07 <saif67090707@gmail.com>
2022-08-02 13:18:58 +0600

    all orders delete

--- index.js
+++ index.js
@@ -220,6 +220,12 @@ async function run() {
       const result = await userCollection.deleteOne(query);
       res.send(result);
     });
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
